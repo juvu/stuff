677f27965c00dca7b7a789e378ce702ca25fa149
antdix <antdix@googlemail.com>
2020-07-04 11:36:10 +0100

    latest

--- python/croquet2.py
+++ python/croquet2.py
@@ -26,6 +26,10 @@ class MyGrid(Widget):
     yellow = ObjectProperty(None)
     blue=ObjectProperty(None)
     black=ObjectProperty(None)
+    redLabel = ObjectProperty(None)
+    yellowLabel = ObjectProperty(None)
+    blueLabel=ObjectProperty(None)
+    blackLabel=ObjectProperty(None)
     win=ObjectProperty(None)
     score=ObjectProperty(None)
     draw=ObjectProperty(None)
@@ -36,6 +40,11 @@ class MyGrid(Widget):
     blackVal = 0
     RYscore = 0
     BKscore = 0
+    colours = 0
+    blueColours = ['Blue', 'Green']
+    blackColours = ['Black', 'Brown']
+    redColours = ['Red', 'White']
+    yellowColours = ['Yellow', 'Pink']
 
     def hoopName(self, hoop):
         if (hoop == 0):
@@ -117,9 +126,27 @@ class MyGrid(Widget):
         self.blackVal = self.hoopVal(value)
         self.BKscore = self.blueVal + self.blackVal
 
+    def spinnerColours(self, value):
+        if (value == "Secondary"):
+            self.redLabel.text = "White"
+            self.yellowLabel.text = "Pink"
+            self.blueLabel.text = "Green"
+            self.blackLabel.text = "Brown"
+            self.colours = 1
+        elif (value == "Primary"):
+            self.redLabel.text = "Red"
+            self.yellowLabel.text = "Yellow"
+            self.blueLabel.text = "Blue"
+            self.blackLabel.text = "Black"
+            self.colours = 0
+        self.btn()
+
     def btn(self):
         if (self.RYscore > self.BKscore):
-            self.score.text = "Score:\nRY {0} BK {1}      RY +{2}".format(self.RYscore, self.BKscore, self.RYscore - self.BKscore)
+            if (self.colours == 0):
+                self.score.text = "Score:\nRed/Yellow {0}    Blue/Black {1}\nRed/Yellow +{2}".format(self.RYscore, self.BKscore, self.RYscore - self.BKscore)
+            else:
+                self.score.text = "Score:\nWhite/Pink {0}    Green/Brown {1}\nWhite/Pink +{2}".format(self.RYscore, self.BKscore, self.RYscore - self.BKscore)
             pointsNeeded = self.RYscore - self.BKscore + 1
             toWin1 = "none"
             toWin2 = "none"
@@ -127,31 +154,30 @@ class MyGrid(Widget):
             toWin4 = "none"
             if (self.blueVal + pointsNeeded < 13):
                 finalPos = self.hoopName(self.blueVal + pointsNeeded)
-                toWin1 = "B to {0}".format(finalPos)
+                toWin1 = "{0} to {1}".format(self.blueColours[self.colours],finalPos)
             elif (self.blueVal + pointsNeeded == 13):
-                toWin1 = "B to Box"
+                toWin1 = "{0} to Box".format(self.blueColours[self.colours])
                 peelFinalPos = self.hoopName(self.blackVal + 1)
-                toWin3 = "B to Peg, K 1 peel to {0}".format(peelFinalPos)
+                toWin3 = "{0} to Peg, {1} 1 peel to {2}".format(self.blueColours[self.colours], self.blackColours[self.colours],peelFinalPos)
             else:
                 peelsNeeded = self.blueVal + pointsNeeded - 13;
                 peelFinalPos = self.hoopName(self.blackVal + peelsNeeded);
-                toWin1 = "B to Box, K {0} peels to {1}".format(peelsNeeded, peelFinalPos)
+                toWin1 = "{0} to Box, {1} {2} peels to {3}".format(self.blueColours[self.colours], self.blackColours[self.colours],peelsNeeded, peelFinalPos)
                 peelFinalPos = self.hoopName(self.blackVal + peelsNeeded + 1)
-                toWin3 = "B to Peg, K {0} peels to {1}".format(peelsNeeded + 1, peelFinalPos)
-
-            if (self.blackVal + pointsNeeded <= 13):
+                toWin3 = "{0} to Peg, {1} {2} peels to {3}".format(self.blueColours[self.colours], self.blackColours[self.colours],peelsNeeded + 1, peelFinalPos)
+            if (self.blackVal + pointsNeeded < 13):
                 finalPos = self.hoopName(self.blackVal + pointsNeeded)
-                toWin2 = "K to {0}".format(finalPos)
+                toWin2 = "{0} to {1}".format(self.blackColours[self.colours],finalPos)
             elif (self.blackVal + pointsNeeded == 13):
-                toWin2 = "K to Box";
+                toWin2 = "{0} to Box".format(self.blackColours[self.colours])
                 peelFinalPos = self.hoopName(self.blueVal + 1)
-                toWin4 = "K to Peg, B 1 peel to {0}".format(peelFinalPos)
+                toWin4 = "{0} to Peg, {1} 1 peel to {2}".format(self.blackColours[self.colours], self.blueColours[self.colours],peelFinalPos)
             else:
-                peelsNeeded = self.blackVal + pointsNeeded - 13
-                peelFinalPos = self.hoopName(self.blueVal + peelsNeeded)
-                toWin2 = "K to Box, B {0} peels to {1}".format(peelsNeeded, peelFinalPos)
+                peelsNeeded = self.blackVal + pointsNeeded - 13;
+                peelFinalPos = self.hoopName(self.blueVal + peelsNeeded);
+                toWin2 = "{0} to Box, {1} {2} peels to {3}".format(self.blackColours[self.colours], self.blueColours[self.colours],peelsNeeded, peelFinalPos)
                 peelFinalPos = self.hoopName(self.blueVal + peelsNeeded + 1)
-                toWin4 = "K to Peg, B {0} peels to {1}".format(peelsNeeded + 1, peelFinalPos)
+                toWin4 = "{0} to Peg, {1} {2} peels to {3}".format(self.blackColours[self.colours], self.blueColours[self.colours],peelsNeeded + 1, peelFinalPos)
 
             if (toWin3 != "none" and toWin4 != "none"):
                 self.win.text = "To win by 1:\n1) {0}\n2) {1}\n3) {2}\n4) {3}".format(toWin1, toWin2, toWin3, toWin4)
@@ -163,7 +189,10 @@ class MyGrid(Widget):
                 self.win.text = "To win by 1:\n1) {0}\n2) {1}\n".format(toWin1, toWin2)
 
         elif (self.BKscore > self.RYscore):
-            self.score.text = "Score:\nRY {0} BK {1}      BK +{2}".format(self.RYscore, self.BKscore, self.BKscore - self.RYscore)
+            if (self.colours == 0):
+                self.score.text = "Score:\nRed/Yellow {0}    Blue/Black {1}\nBlue/Black +{2}".format(self.RYscore, self.BKscore, self.BKscore - self.RYscore)
+            else:
+                self.score.text = "Score:\nWhite/Pink {0}    Green/Brown {1}\nGreen/Brown +{2}".format(self.RYscore, self.BKscore, self.BKscore - self.RYscore)
             pointsNeeded = self.BKscore - self.RYscore + 1
             toWin1 = "none"
             toWin2 = "none"
@@ -171,31 +200,30 @@ class MyGrid(Widget):
             toWin4 = "none"
             if (self.redVal + pointsNeeded < 13):
                 finalPos = self.hoopName(self.redVal + pointsNeeded)
-                toWin1 = "R to {0}".format(finalPos)
+                toWin1 = "{0} to {1}".format(self.redColours[self.colours],finalPos)
             elif (self.redVal + pointsNeeded == 13):
-                toWin1 = "R to Box"
+                toWin1 = "{0} to Box".format(self.redColours[self.colours])
                 peelFinalPos = self.hoopName(self.yellowVal + 1)
-                toWin3 = "R to Peg, Y 1 peel to {0}".format(peelFinalPos)
+                toWin3 = "{0} to Peg, {1} 1 peel to {2}".format(self.redColours[self.colours], self.yellowColours[self.colours],peelFinalPos)
             else:
                 peelsNeeded = self.redVal + pointsNeeded - 13;
                 peelFinalPos = self.hoopName(self.yellowVal + peelsNeeded);
-                toWin1 = "R to Box, Y {0} peels to {1}".format(peelsNeeded, peelFinalPos)
-                peelFinalPos = self.hoopName(self.yellowkVal + peelsNeeded + 1)
-                toWin3 = "R to Peg, Y {0} peels to {1}".format(peelsNeeded + 1, peelFinalPos)
-
-            if (self.yellowVal + pointsNeeded <= 13):
+                toWin1 = "{0} to Box, {1} {2} peels to {3}".format(self.redColours[self.colours], self.yellowColours[self.colours],peelsNeeded, peelFinalPos)
+                peelFinalPos = self.hoopName(self.yellowVal + peelsNeeded + 1)
+                toWin3 = "{0} to Peg, {1} {2} peels to {3}".format(self.redColours[self.colours], self.yellowColours[self.colours],peelsNeeded + 1, peelFinalPos)
+            if (self.yellowVal + pointsNeeded < 13):
                 finalPos = self.hoopName(self.yellowVal + pointsNeeded)
-                toWin2 = "Y to {0}".format(finalPos)
+                toWin2 = "{0} to {1}".format(self.yellowColours[self.colours],finalPos)
             elif (self.yellowVal + pointsNeeded == 13):
-                toWin2 = "Y to Box";
+                toWin2 = "{0} to Box".format(self.yellowColours[self.colours])
                 peelFinalPos = self.hoopName(self.redVal + 1)
-                toWin4 = "Y to Peg, R 1 peel to {0}".format(peelFinalPos)
+                toWin4 = "{0} to Peg, {1} 1 peel to {2}".format(self.yellowColours[self.colours], self.redColours[self.colours],peelFinalPos)
             else:
-                peelsNeeded = self.yellowVal + pointsNeeded - 13
-                peelFinalPos = self.hoopName(self.redVal + peelsNeeded)
-                toWin2 = "Y to Box, R {0} peels to {1}".format(peelsNeeded, peelFinalPos)
-                peelFinalPos = self.hoopName(self.blueVal + peelsNeeded + 1)
-                toWin4 = "Y to Peg, R {0} peels to {1}".format(peelsNeeded + 1, peelFinalPos)
+                peelsNeeded = self.yellowVal + pointsNeeded - 13;
+                peelFinalPos = self.hoopName(self.redVal + peelsNeeded);
+                toWin2 = "{0} to Box, {1} {2} peels to {3}".format(self.yellowColours[self.colours], self.redColours[self.colours],peelsNeeded, peelFinalPos)
+                peelFinalPos = self.hoopName(self.redVal + peelsNeeded + 1)
+                toWin4 = "{0} to Peg, {1} {2} peels to {3}".format(self.yellowColours[self.colours], self.redColours[self.colours],peelsNeeded + 1, peelFinalPos)
 
             if (toWin3 != "none" and toWin4 != "none"):
                 self.win.text = "To win by 1:\n1) {0}\n2) {1}\n3) {2}\n4) {3}".format(toWin1, toWin2, toWin3, toWin4)
@@ -207,9 +235,11 @@ class MyGrid(Widget):
                 self.win.text = "To win by 1:\n1) {0}\n2) {1}\n".format(toWin1, toWin2)
 
         elif (self.BKscore == self.RYscore):
-            self.score.text = "Score:\nRY {0} BK {1}      Score tied".format(self.RYscore, self.BKscore)
+            if (self.colours == 0):
+                self.score.text = "Score:\nRed/Yellow {0}    Blue/Black {1}\nScore tied".format(self.RYscore, self.BKscore)
+            else:
+                self.score.text = "Score:\nWhite/Pink {0}    Green/Brown {1}\nScore tied".format(self.RYscore, self.BKscore)
             self.win.text = "Golden Hoop\n"
-
 
         if (self.RYscore > self.BKscore):
             pointsNeeded = self.RYscore - self.BKscore
@@ -219,31 +249,30 @@ class MyGrid(Widget):
             toWin4 = "none"
             if (self.blueVal + pointsNeeded < 13):
                 finalPos = self.hoopName(self.blueVal + pointsNeeded)
-                toWin1 = "B to {0}".format(finalPos)
+                toWin1 = "{0} to {1}".format(self.blueColours[self.colours],finalPos)
             elif (self.blueVal + pointsNeeded == 13):
-                toWin1 = "B to Box"
+                toWin1 = "{0} to Box".format(self.blueColours[self.colours])
                 peelFinalPos = self.hoopName(self.blackVal + 1)
-                toWin3 = "B to Peg, K 1 peel to {0}".format(peelFinalPos)
+                toWin3 = "{0} to Peg, {1} 1 peel to {2}".format(self.blueColours[self.colours], self.blackColours[self.colours],peelFinalPos)
             else:
                 peelsNeeded = self.blueVal + pointsNeeded - 13;
                 peelFinalPos = self.hoopName(self.blackVal + peelsNeeded);
-                toWin1 = "B to Box, K {0} peels to {1}".format(peelsNeeded, peelFinalPos)
+                toWin1 = "{0} to Box, {1} {2} peels to {3}".format(self.blueColours[self.colours], self.blackColours[self.colours],peelsNeeded, peelFinalPos)
                 peelFinalPos = self.hoopName(self.blackVal + peelsNeeded + 1)
-                toWin3 = "B to Peg, K {0} peels to {1}".format(peelsNeeded + 1, peelFinalPos)
-
-            if (self.blackVal + pointsNeeded <= 13):
+                toWin3 = "{0} to Peg, {1} {2} peels to {3}".format(self.blueColours[self.colours], self.blackColours[self.colours],peelsNeeded + 1, peelFinalPos)
+            if (self.blackVal + pointsNeeded < 13):
                 finalPos = self.hoopName(self.blackVal + pointsNeeded)
-                toWin2 = "K to {0}".format(finalPos)
+                toWin2 = "{0} to {1}".format(self.blackColours[self.colours],finalPos)
             elif (self.blackVal + pointsNeeded == 13):
-                toWin2 = "K to Box";
+                toWin2 = "{0} to Box".format(self.blackColours[self.colours])
                 peelFinalPos = self.hoopName(self.blueVal + 1)
-                toWin4 = "K to Peg, B 1 peel to {0}".format(peelFinalPos)
+                toWin4 = "{0} to Peg, {1} 1 peel to {2}".format(self.blackColours[self.colours], self.blueColours[self.colours],peelFinalPos)
             else:
-                peelsNeeded = self.blackVal + pointsNeeded - 13
-                peelFinalPos = self.hoopName(self.blueVal + peelsNeeded)
-                toWin2 = "K to Box, B {0} peels to {1}".format(peelsNeeded, peelFinalPos)
+                peelsNeeded = self.blackVal + pointsNeeded - 13;
+                peelFinalPos = self.hoopName(self.blueVal + peelsNeeded);
+                toWin2 = "{0} to Box, {1} {2} peels to {3}".format(self.blackColours[self.colours], self.blueColours[self.colours],peelsNeeded, peelFinalPos)
                 peelFinalPos = self.hoopName(self.blueVal + peelsNeeded + 1)
-                toWin4 = "K to Peg, B {0} peels to {1}".format(peelsNeeded + 1, peelFinalPos)
+                toWin4 = "{0} to Peg, {1} {2} peels to {3}".format(self.blackColours[self.colours], self.blueColours[self.colours],peelsNeeded + 1, peelFinalPos)
 
             if (toWin3 != "none" and toWin4 != "none"):
                 self.draw.text = "To draw level:\n1) {0}\n2) {1}\n3) {2}\n4) {3}".format(toWin1, toWin2, toWin3, toWin4)
@@ -262,31 +291,30 @@ class MyGrid(Widget):
             toWin4 = "none"
             if (self.redVal + pointsNeeded < 13):
                 finalPos = self.hoopName(self.redVal + pointsNeeded)
-                toWin1 = "R to {0}".format(finalPos)
+                toWin1 = "{0} to {1}".format(self.redColours[self.colours],finalPos)
             elif (self.redVal + pointsNeeded == 13):
-                toWin1 = "R to Box"
+                toWin1 = "{0} to Box".format(self.redColours[self.colours])
                 peelFinalPos = self.hoopName(self.yellowVal + 1)
-                toWin3 = "R to Peg, Y 1 peel to {0}".format(peelFinalPos)
+                toWin3 = "{0} to Peg, {1} 1 peel to {2}".format(self.redColours[self.colours], self.yellowColours[self.colours],peelFinalPos)
             else:
                 peelsNeeded = self.redVal + pointsNeeded - 13;
                 peelFinalPos = self.hoopName(self.yellowVal + peelsNeeded);
-                toWin1 = "R to Box, Y {0} peels to {1}".format(peelsNeeded, peelFinalPos)
-                peelFinalPos = self.hoopName(self.yellowkVal + peelsNeeded + 1)
-                toWin3 = "R to Peg, Y {0} peels to {1}".format(peelsNeeded + 1, peelFinalPos)
-
-            if (self.yellowVal + pointsNeeded <= 13):
+                toWin1 = "{0} to Box, {1} {2} peels to {3}".format(self.redColours[self.colours], self.yellowColours[self.colours],peelsNeeded, peelFinalPos)
+                peelFinalPos = self.hoopName(self.yellowVal + peelsNeeded + 1)
+                toWin3 = "{0} to Peg, {1} {2} peels to {3}".format(self.redColours[self.colours], self.yellowColours[self.colours],peelsNeeded + 1, peelFinalPos)
+            if (self.yellowVal + pointsNeeded < 13):
                 finalPos = self.hoopName(self.yellowVal + pointsNeeded)
-                toWin2 = "Y to {0}".format(finalPos)
+                toWin2 = "{0} to {1}".format(self.yellowColours[self.colours],finalPos)
             elif (self.yellowVal + pointsNeeded == 13):
-                toWin2 = "Y to Box";
+                toWin2 = "{0} to Box".format(self.yellowColours[self.colours])
                 peelFinalPos = self.hoopName(self.redVal + 1)
-                toWin4 = "Y to Peg, R 1 peel to {0}".format(peelFinalPos)
+                toWin4 = "{0} to Peg, {1} 1 peel to {2}".format(self.yellowColours[self.colours], self.redColours[self.colours],peelFinalPos)
             else:
-                peelsNeeded = self.yellowVal + pointsNeeded - 13
-                peelFinalPos = self.hoopName(self.redVal + peelsNeeded)
-                toWin2 = "Y to Box, R {0} peels to {1}".format(peelsNeeded, peelFinalPos)
-                peelFinalPos = self.hoopName(self.blueVal + peelsNeeded + 1)
-                toWin4 = "Y to Peg, R {0} peels to {1}".format(peelsNeeded + 1, peelFinalPos)
+                peelsNeeded = self.yellowVal + pointsNeeded - 13;
+                peelFinalPos = self.hoopName(self.redVal + peelsNeeded);
+                toWin2 = "{0} to Box, {1} {2} peels to {3}".format(self.yellowColours[self.colours], self.redColours[self.colours],peelsNeeded, peelFinalPos)
+                peelFinalPos = self.hoopName(self.redVal + peelsNeeded + 1)
+                toWin4 = "{0} to Peg, {1} {2} peels to {3}".format(self.yellowColours[self.colours], self.redColours[self.colours],peelsNeeded + 1, peelFinalPos)
 
             if (toWin3 != "none" and toWin4 != "none"):
                 self.draw.text = "To draw level:\n1) {0}\n2) {1}\n3) {2}\n4) {3}".format(toWin1, toWin2, toWin3, toWin4)
@@ -296,6 +324,7 @@ class MyGrid(Widget):
                 self.draw.text = "To draw level:\n1) {0}\n2) {1}\n3) {2}".format(toWin1, toWin2, toWin4)
             else:
                 self.draw.text = "To draw level:\n1) {0}\n2) {1}\n".format(toWin1, toWin2)
+
 
 class MyApp(App):
     def build(self):
